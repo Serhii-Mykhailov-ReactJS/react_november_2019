897e199d85eab3b306ec5829240c2128bdedf1d5
Kirill Biezdielnyi <ghostkirill@gmail.com>
2019-11-06 14:39:52 +0200

    Empty create-react-app first commit

---
+++ lesson_01/src/components/App/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../logo.png';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Homework 1 (empty create-react-app with simple structure)
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
