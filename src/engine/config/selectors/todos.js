749e4a6b178efd3090b81db8502507fad448c912
serhii <mikhajlov87@gmail.com>
2019-11-28 22:43:01 +0200

    add materials to lesson_8

--- src/engine/config/selectors/todos.js
+++ src/engine/config/selectors/todos.js
@@ -1,3 +1,26 @@
+// Core
+import { createSelector } from 'reselect';
+
+const todosSelector = state => state.todos.todos;
+
+const todosDoneSelector = createSelector(
+  todosSelector,
+  (todos) => {
+    return todos.filter(item => item.done);
+  },
+);
+
+const firstNameSelector = state => state.user.firstName;
+const lastNameSelector = state => state.user.lastName;
+
+const fullNameSelector = createSelector(
+  firstNameSelector,
+  lastNameSelector,
+  (firstName, lastName) => {
+    return `${firstName} ${lastName}`;
+  },
+);
+
 export const todosSelectors = Object.freeze({
   todoList: state => state.todos.todos || [],
 });
